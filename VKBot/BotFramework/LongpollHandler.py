f99e7a0669cab0416c9701436e7606b31a5175a3
Dmitry <43281522+KartonDCP@users.noreply.github.com>
2020-04-20 20:31:54 +0300

    probably correct code ;?

--- VKBot/BotFramework/LongpollHandler.py
+++ VKBot/BotFramework/LongpollHandler.py
@@ -3,7 +3,9 @@ import inspect
 from vk_api import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 
-from Controllers import ChatMsgController
+from BotFramework.EventSender import ChatEventSender
+from BotFramework.VkAction import VkAction
+from Controllers.ChatMsgController import ChatMsgController
 
 
 class LongPollHandler:
@@ -12,12 +14,24 @@ class LongPollHandler:
         self._vk_session = vk_api.VkApi(token=token)
         self._session_api = self._vk_session.get_api()
         self._long_poll = VkLongPoll(self._vk_session)
+        self._vk_action = VkAction(token)
+
+        self.chat_controller = ChatMsgController(self._vk_action)
+
+        self.chat_handlers = self._methods_with_decorator(ChatMsgController, "HandleMessage")
 
     def start_handle(self):
         for event in self._long_poll.listen():
             if event.type == VkEventType.MESSAGE_NEW:
                 if event.from_chat:
-                    pass    # UserController Startup
+                    # trying find handler for message
+                    for handler in self.chat_handlers:
+                        if str(handler[1]).split('=')[1].replace('"', '').replace(' ', '') == event.text:
+                            chat_event = ChatEventSender(event.chat_id,
+                                                         int(event.extra_values['from']),
+                                                         {"message": event.text,
+                                                          "attachment": event.attachments})
+                            getattr(self.chat_controller, handler[0])(chat_event)   # Call method by name
                 if event.from_user:
                     pass    # ChatController Startup
                 if event.from_group:
@@ -29,19 +43,26 @@ class LongPollHandler:
         pass
 
     @staticmethod
-    def _methods_with_decorator(controller_name: str, decorator_name: str):
+    def _methods_with_decorator(controller_name, decorator_name: str) -> GeneratorExit(str):
+        """
+
+        :type controller_name: PATH
+        """
+        # raise Warning("\"_methods_with_decorator\" Method works with errors! do unit tests and rewrite it")
         source_lines = inspect.getsourcelines(controller_name)[0]
         for i, line in enumerate(source_lines):
             line = line.strip()
             if line.split('(')[0].strip() == '@' + decorator_name:
                 param = line.split('(')[1].split(')')[0]
-                next_line = source_lines[i+1]
+                next_line = source_lines[i + 1]
+                while '@' in next_line:
+                    next_line = source_lines[i+1]
                 decor_name = next_line.split('def')[1].split('(')[0].strip()
                 yield (decor_name, param)
 
 
-for name in LongPollHandler._methods_with_decorator(ChatMsgController, "Authorized"):
-    print(name)
 
+longpoll = LongPollHandler('7328f96c2d8997c8bbaffc4b53e78cbab374ecf6339f3943af4ef65b9da1db589aa831f04ba36e34da46e')
+longpoll.start_handle()
 
 
