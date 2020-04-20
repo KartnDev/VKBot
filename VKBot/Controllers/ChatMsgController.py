2a23db2eb4f3877b5faeabe5cf0f195d6213868e
Dmitry <43281522+KartonDCP@users.noreply.github.com>
2020-04-20 21:09:08 +0300

    Update ChatMsgController.py

--- VKBot/Controllers/ChatMsgController.py
+++ VKBot/Controllers/ChatMsgController.py
@@ -11,21 +11,22 @@ class ChatMsgController:
     def __init__(self, vk_action: VkAction):
         self.vk = vk_action
 
-    @Authorized()
-    @RequiredLvl(lvl=10)
+    @HandleMessage(msg="!Крыса")
+    def handle_krisa(self, event_sender: ChatEventSender):
+        self.vk.send_message_chat(event_sender.chat_id, "САМ ТЫ КРЫСА @id" + str(event_sender.user_id))
+
     @HandleMessage(msg="!Hello")
     def handle_name(self, event_sender: ChatEventSender):
         self.vk.send_message_chat(event_sender.chat_id, "ИДИ НАХУЙ!")
-        print(event_sender.chat_id)
-        print(event_sender.user_id)
-        print(event_sender.event['message'])
+
+        print("Сообщение пришло из чата id"+ str(event_sender.chat_id))
+        print("Сообщение пришло от юзера id"+ str(event_sender.user_id))
+        print("Текст сообщенияЖ :"+ event_sender.event['message'])
         print(event_sender.event['attachment'])
 
-    @Authorized()
+
     @HandleMessage(msg="!Bye")
     def retw_TO(self, event_sender: ChatEventSender):
         self.vk.send_message('Hello from bot')
-        print(event_sender.chat_id)
-        print(event_sender.user_id)
-        print(event_sender.event['message'])
-        print(event_sender.event['attachment'])
+
+
