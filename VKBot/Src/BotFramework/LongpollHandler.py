92854b50a5d6cb9290c1d37e388d37cd5a14d84a
Dmitry <43281522+KartonDCP@users.noreply.github.com>
2020-05-18 21:32:42 +0300

    Update LongpollHandler.py

--- VKBot/Src/BotFramework/LongpollHandler.py
+++ VKBot/Src/BotFramework/LongpollHandler.py
@@ -7,6 +7,7 @@ from Src.BotFramework.EventSender import ChatEventSender
 from Src.BotFramework.VkAction import VkAction
 from Src.Controllers.ChatMsgController import ChatMsgController
 from Src.Vk.LongPollListener.LongpollListener import LongPollListener
+from Src.Database.UserDbWorker import UserDbWorker
 from Src.Vk.VkApiCore import VkCore
 
 
@@ -15,11 +16,17 @@ class LongPollHandler:
     def __init__(self, vk_api_core: VkCore):
         self._long_poll = LongPollListener(vk_api_core)
         self._vk_action = VkAction(vk_api_core)
-        self.chat_controller = ChatMsgController(self._vk_action)
-        self.chat_handlers = self._methods_with_decorator(ChatMsgController, "HandleMessage")
 
-    async def callback_listener(self):
-        pass
+        self.chat_controller = ChatMsgController(self._vk_action)
+
+        self.chat_handlers = self._methods_with_decorator(ChatMsgController, "HandleMessage")
+        self.chat_required_level_handlers = self._methods_with_decorator(ChatMsgController, "RequiredLvl")
+        self.chat_auth_handlers = self._methods_with_decorator(ChatMsgController, "Authorized")
+
+        self._user_wrr = UserDbWorker()
+
+    def _send_call_error_chat(self, chat_id: int, msg: str):
+        return self._vk_action.send_message_chat(chat_id=chat_id, message="Call error: " + msg)
 
     def start_handle(self):
         for event in self._long_poll.listen():
@@ -30,13 +37,31 @@ class LongPollHandler:
                         if msg['peer_id'] > int(2E9):   # from_chat
                             # trying find handler for message
                             for _handler in self.chat_handlers:
-                                msg_handle = _handler[1].split('=')[1].replace('\"', '').replace(' ', '').replace('\'', '')
+                                msg_handle = _handler[1].split('=')[1].replace('\"', '').replace(' ', '')\
+                                                                                        .replace('\'', '')
                                 if msg_handle == msg['text']:
-                                    chat_event = ChatEventSender(msg['peer_id'] - int(2E9),
-                                                                 int(msg['from_id']),
-                                                                 {"message": msg['text'],
-                                                                  "attachment": msg['attachments']})
-                                    getattr(self.chat_controller, _handler[0])(chat_event)   # Call method by name
+                                    current_vk_u = msg['from_id']
+                                    # for AUTH ONLY COMMANDS
+                                    if _handler in self.chat_auth_handlers and self._user_wrr.contains(current_vk_u):
+                                        chat_event = ChatEventSender(msg['peer_id'] - int(2E9),
+                                                                     int(msg['from_id']),
+                                                                     {"message": msg['text'],
+                                                                      "attachment": msg['attachments']})
+                                        getattr(self.chat_controller, _handler[0])(chat_event)   # Call method by name
+                                        
+                                    else:
+                                        self._send_call_error_chat(msg['peer_id'] - int(2E9),
+                                                                   """комманда доступна только для зарегестрированных 
+                                                                   пользователей""")
+                                    # for Level - required COMMANDS
+                                    if handler in self.chat_required_level_handlers:
+                                        curr_u_lvl = self._user_wrr.select_one(current_vk_u)['access_level']
+                                        lvl_handle = int(_handler[1].split('=')[1].replace('\"', '').replace(' ', '')\
+                                                                                            .replace('\'', ''))
+                                        if current_vk_u >= lvl_handle:
+
+
+                        elif msg['peer_id'] < int(2E9):
 
     def _find_def_with(self, msg: str, decorator: str, controller_name: str):
         pass
@@ -61,5 +86,9 @@ class LongPollHandler:
     def load_stateless_controllers(self, typename: str) -> list:
         pass
 
+
+core = VkCore('', 'cdefe64cad4dfb777159fed5802a6a85ddc7a29eaa4e7f6e876096a07ce53887baa982487b8883b964f8d')
+handler = LongPollHandler(core)
+handler.start_handle()
 
 
