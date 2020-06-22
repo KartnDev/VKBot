23c1c1fb19f6fbd769531863a73e99da2eee0587
Dmitry <43281522+KartonDCP@users.noreply.github.com>
2020-06-22 23:11:06 +0300

    Update LongpollLikeListener.py

--- VKBot/Src/BotFramework/Telergam/TelegramListener/LongpollLikeListener.py
+++ VKBot/Src/BotFramework/Telergam/TelegramListener/LongpollLikeListener.py
@@ -11,25 +11,32 @@ class TelegramListener(TelegramAction):
 
     async def listen_events(self):
         _res: dict = self.get_updates_json()
-        if 'ok' in _res and 'result' in _res:
-            if _res['ok']:
-                if 'update_id' in _res['result'][-1]:
-                    last_update_id = _res['result'][-1]['update_id']
-                    while True:
-                        _new_res = self.get_updates_json()
-                        if last_update_id < _new_res['result'][-1]['update_id']:
+        result = self.__valid(_res)
 
-                            delta_last = _new_res['result'][-1]['update_id'] - last_update_id
-                            if delta_last == 1:
-                                print(_new_res['result'][-1]['message']['text'])
-                            elif delta_last > 1:
-                                for item in _new_res['result'][-delta_last:]:
-                                    print(item['message']['text'])
-                            last_update_id = _new_res['result'][-1]['update_id']
-                        await asyncio.sleep(1)
+        if result[0]:
+            last_update_id = result[1]['update_id']
+            while True:
+                _new_res = self.__valid(self.get_updates_json())
+                if _new_res[0]:
+                    if last_update_id < _new_res[1]['update_id']:
+                        delta_last = _new_res[1]['update_id'] - last_update_id
+                        if delta_last == 1:
+                            print(_new_res[1]['message']['text'])
+                        elif delta_last > 1:
+                            for item in _new_res['result'][-delta_last:]:
+                                print(item['message']['text'])
+                        last_update_id = _new_res['result'][-1]['update_id']
+                    await asyncio.sleep(1)
 
-
-
-
+    @staticmethod
+    def __valid(event: dict, sliced: int = -1) -> (bool, dict):
+        if 'ok' in event and 'result' in event:
+            if event['ok']:
+                if isinstance(event['result'], list):
+                    if len(event['result']) > 0:
+                        last_event = event['result'][sliced]
+                        if 'update_id' in last_event:
+                            return True, last_event
+        return False, None
 
 
