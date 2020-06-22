93cabf8aeb7f76d949ecff0df46bd8aa2e24e568
Dmitry <43281522+KartonDCP@users.noreply.github.com>
2020-06-22 23:23:21 +0300

    add validationfucntion for json

--- VKBot/Src/BotFramework/Telergam/TelegramListener/LongpollLikeListener.py
+++ VKBot/Src/BotFramework/Telergam/TelegramListener/LongpollLikeListener.py
@@ -11,32 +11,31 @@ class TelegramListener(TelegramAction):
 
     async def listen_events(self):
         _res: dict = self.get_updates_json()
-        result = self.__valid(_res)
-
-        if result[0]:
-            last_update_id = result[1]['update_id']
+        if self.__valid(_res):
+            last_update_id = _res['result'][-1]['update_id']
             while True:
-                _new_res = self.__valid(self.get_updates_json())
-                if _new_res[0]:
-                    if last_update_id < _new_res[1]['update_id']:
-                        delta_last = _new_res[1]['update_id'] - last_update_id
-                        if delta_last == 1:
-                            print(_new_res[1]['message']['text'])
-                        elif delta_last > 1:
-                            for item in _new_res['result'][-delta_last:]:
-                                print(item['message']['text'])
-                        last_update_id = _new_res['result'][-1]['update_id']
-                    await asyncio.sleep(1)
+                _new_res = self.get_updates_json()
+                if last_update_id < _new_res[1]['update_id']:
+                    delta_last = _new_res[1]['update_id'] - last_update_id
+                    if delta_last == 1:
+                        print(_new_res[1]['message']['text'])
+                        # await yield _new_res[1]['message']['text']
+                    elif delta_last > 1:
+                        for item in _new_res['result'][-delta_last:]:
+                            print(item['message']['text'])
+                            # await yield item['message']['text']
+                    last_update_id = _new_res['result'][-1]['update_id']
+                await asyncio.sleep(1)
 
     @staticmethod
-    def __valid(event: dict, sliced: int = -1) -> (bool, dict):
+    def __valid(event: dict, sliced: int = -1) -> bool:
         if 'ok' in event and 'result' in event:
             if event['ok']:
                 if isinstance(event['result'], list):
                     if len(event['result']) > 0:
                         last_event = event['result'][sliced]
                         if 'update_id' in last_event:
-                            return True, last_event
-        return False, None
+                            return True
+        return False
 
 
--- VKBot/Src/BotFramework/Telergam/TelegramWorker.py
+++ VKBot/Src/BotFramework/Telergam/TelegramWorker.py
@@ -1,2 +1,3 @@
 class TelegramWorker:
-    def __init__(self, ):
+    def __init__(self):
+        pass
