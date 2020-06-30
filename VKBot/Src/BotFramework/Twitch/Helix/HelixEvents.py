5817089985099a2f763ca3c68ad5bf2afa83084f
Dmitry <43281522+KartonDCP@users.noreply.github.com>
2020-06-30 22:48:28 +0300

    add status for streams

--- VKBot/Src/BotFramework/Twitch/Helix/HelixEvents.py
+++ VKBot/Src/BotFramework/Twitch/Helix/HelixEvents.py
@@ -1,3 +1,6 @@
+import datetime
+
+
 class ChannelInfo:
     def __init__(self, json_res: dict):
         self._json_data = json_res
@@ -11,3 +14,28 @@ class ChannelInfo:
         self.broadcaster_language = json_res['broadcaster_language']
         self.stream_title = json_res['title']
         self.broadcaster_name = json_res['broadcaster_name']
+
+
+class StreamInfo:
+    def __init__(self, json_res: dict):
+        self._json_res = json_res
+        self.broadcaster_language: str = json_res['broadcaster_language']
+        self.streamer_nickname: str = json_res['display_name']
+        self.game_id: int = int(json_res['game_id'])
+        self.id: int = int(json_res['id'])
+        self.is_life: bool = json_res['is_live']
+        self.tag_guid_ids: str = json_res['tag_ids']
+        self.thumbnail_url: str = json_res['thumbnail_url']
+        self.stream_title: str = json_res['title']
+        self.stream_started_at: str = json_res['started_at']
+
+    def __str__(self):
+        return """
+        streamer_nickname: {0}
+        is_streaming: {1}
+        stream_title: {2}
+        broadcaster_language: {3}
+        """.format(self.streamer_nickname, self.is_life, self.stream_title, self.broadcaster_language)
+
+    def get_started_at_as_datetime(self) -> datetime.datetime:
+        return datetime.datetime.strptime(self.stream_started_at, "%Y-%m-%dT%H:%M:%SZ")
--- VKBot/Src/BotFramework/Twitch/TwitchActioner.py
+++ VKBot/Src/BotFramework/Twitch/TwitchActioner.py
@@ -1,4 +1,4 @@
-from Src.BotFramework.Twitch.Helix.HelixEvents import ChannelInfo
+from Src.BotFramework.Twitch.Helix.HelixEvents import ChannelInfo, StreamInfo
 from Src.BotFramework.Twitch.TwitchCore import TwitchCore
 
 
@@ -6,7 +6,7 @@ class TwitchAction:
     def __init__(self, twitch_api_core: TwitchCore):
         self._twitch_api_core = twitch_api_core
 
-    def get_channel_info(self, broadcaster_id: int or str):
+    def get_channel_info(self, broadcaster_id: int or str) -> ChannelInfo:
         _res = self._twitch_api_core.helix_request(method_name='channels',
                                                    uri_args={'broadcaster_id': broadcaster_id})
         _res_json: dict = _res.json()
@@ -20,6 +20,50 @@ class TwitchAction:
         elif _res.status_code == 500:
             print('Internal Server Error; Failed to get channel information')
 
+    async def get_channel_info_async(self, broadcaster_id: int or str):
+        _res = await self._twitch_api_core.helix_request_async(method_name='channels',
+                                                               uri_args={'broadcaster_id': broadcaster_id})
+        _res_json: dict = _res.json()
+        if _res.status_code == 200:
+            if 'data' in _res_json:
+                return ChannelInfo(_res_json['data'][0])
+            else:
+                print('no data in response')
+        elif _res.status_code == 400:
+            print("missing query params")
+        elif _res.status_code == 500:
+            print('Internal Server Error; Failed to get channel information')
 
+    def _search_channels_query(self, query: str):
+        return self._twitch_api_core.helix_request(method_name='search/channels', uri_args={'query': query})
 
+    async def _search_channels_query_async(self, query: str):
+        return await self._twitch_api_core.helix_request(method_name='search/channels', uri_args={'query': query})
+
+    def get_stream_info(self, channel_name: str) -> StreamInfo:
+        _res = self._search_channels_query(channel_name)
+        if _res.status_code == 200:
+            _res_json = _res.json()
+            if 'data' in _res_json:
+                query_data = _res_json['data']
+                if isinstance(_res_json['data'], list):
+                    if len(_res_json['data']) > 0:
+                        return StreamInfo(_res_json['data'][0])
+
+    async def get_stream_info_async(self, channel_name: str):
+        _res = await self._search_channels_query_async(channel_name)
+        if _res.status_code == 200:
+            _res_json = _res.json()
+            if 'data' in _res_json:
+                query_data = _res_json['data']
+                if isinstance(_res_json['data'], list):
+                    if len(_res_json['data']) > 0:
+                        return StreamInfo(_res_json['data'][0])
+
+    def is_channel_online(self, channel_name: str) -> bool:
+        return self.get_stream_info(channel_name).is_life
+
+    async def is_channel_online_async(self, channel_name: str):
+        info = await self.get_stream_info_async(channel_name)
+        return info.is_life
 
